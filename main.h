490a6ee23e38d02935e0543340952830f5a2725f
Kamil-Grigorovic <kamil.grigorovic@gmail.com>
2025-11-07 09:46:05 +0200

    Add files via upload

--- main.h
+++ main.h
@@ -12,14 +12,37 @@
 using namespace std;
 
 template <typename T>
-struct Studentas {
+class Studentas {
+public:
     using paz_type = T;
+
+private:
     string vard;
     string pav;
     T paz;
     int egzas;
     float rez;
     float mediana;
+
+public:
+    Studentas() : egzas(0), rez(0), mediana(0) {}
+    Studentas(const string& v, const string& p, const T& paz, int e)
+        : vard(v), pav(p), paz(paz), egzas(e) {}
+
+    string getVard() const { return vard; }
+    string getPav() const { return pav; }
+    T getPaz() const { return paz; }
+    int getEgzas() const { return egzas; }
+    float getRez() const { return rez; }
+    float getMediana() const { return mediana; }
+
+    void setVard(const string& v) { vard = v; }
+    void setPav(const string& p) { pav = p; }
+    void setPaz(const T& paz) { this->paz = paz; }
+    void setEgzas(int e) { egzas = e; }
+    void setRez(float r) { rez = r; }
+    void setMediana(float m) { mediana = m; }
+
 };
 
 // Funkciju deklaracijos
