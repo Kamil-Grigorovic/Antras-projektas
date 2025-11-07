9601f02c94e728a9cc04eb2c82703f9f24afa62d
Kamil-Grigorovic <kamil.grigorovic@gmail.com>
2025-11-07 09:53:25 +0200

    Add files via upload

--- functions.cpp
+++ functions.cpp
@@ -3,35 +3,36 @@
 // Funkcija studento ivedimui rankiniu budu
 template <typename T>
 Studentas<T> ivesk() {
-    Studentas<T> Laik;
-    cout << "Ivesk varda: "; 
-    getline(cin, Laik.vard);
-    cout << "Ivesk pavarde: "; 
-    getline(cin, Laik.pav);
-    cout << "Iveskite namu darbu pazymius po viena. Kai baigsite, tiesiog paspauskite ENTER:\n";
-    string temp;
-    Laik.paz.clear();
+    Studentas<T> s;
+    string input;
+    cout << "Iveskite varda: ";
+    getline(cin, input);
+    s.setVard(input);
+    cout << "Iveskite pavarde: ";
+    getline(cin, input);
+    s.setPav(input);
 
+    T paz;
+    cout << "Iveskite namu darbu pazymius po viena. Kai baigsite, paspauskite ENTER:\n";
     while (true) {
         cout << "Pazymys: ";
-        getline(cin, temp);
-        if (temp.empty()) break;
+        getline(cin, input);
+        if (input.empty()) break;
         try {
-            int paz = stoi(temp);
-            Laik.paz.push_back(paz);
+            int pazymys = stoi(input);
+            paz.push_back(pazymys);
         } catch (...) {
-            cout << "Klaida: iveskite skaiciu arba paspauskite ENTER, kad baigti.\n";
+            cout << "Klaida: iveskite skaiciu arba ENTER, kad baigti.\n";
         }
     }
-    if (Laik.paz.empty()) {
-        cout << "Studentas neturi namu darbu. Bus naudojamas 0.\n";
-        Laik.paz.push_back(0);
-    }
+    if (paz.empty()) paz.push_back(0);
+    s.setPaz(paz);
+
     while (true) {
         cout << "Iveskite egzamino bala: ";
-        getline(cin, temp);
+        getline(cin, input);
         try {
-            Laik.egzas = stoi(temp);
+            s.setEgzas(stoi(input));
             break;
         } catch (...) {
             cout << "Klaida: iveskite skaiciu!\n";
@@ -39,10 +40,12 @@ Studentas<T> ivesk() {
     }
 
     int sum = 0;
-    for (int p : Laik.paz) sum += p;
-    Laik.rez = Laik.egzas * 0.6 + (float)sum / Laik.paz.size() * 0.4;
-    Laik.mediana = skaiciuotiMediana(Laik.paz);
-    return Laik;
+    for (int p : paz) sum += p;
+    float rez = s.getEgzas() * 0.6f + (float)sum / paz.size() * 0.4f;
+    s.setRez(rez);
+    s.setMediana(skaiciuotiMediana(paz));
+
+    return s;
 }
 
 // Funkcija vienos eilutes skaitymui is failo
