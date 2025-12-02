b79f53dcbddebe51045e1d00b852ca43720c7ca7
Kamil-Grigorovic <kamil.grigorovic@gmail.com>
2025-12-02 19:11:56 +0200

    Update base.h

--- base.h
+++ base.h
@@ -2,14 +2,37 @@
 
 using namespace std;
 
+/**
+ * @class Zmogus
+ * @brief Bazinė abstrakti klasė, aprašanti žmogų su vardu ir pavarde.
+ *
+ * Ši klasė naudojama kaip pagrindas studentams ar kitiems asmenims.
+ * Turi grynai virtualią funkciją info(), todėl negali būti sukurta tiesiogiai.
+ */
 class Zmogus {
 private:
-    string vard;
-    string pav;
+    string vard; ///< Studento vardas
+    string pav; ///< Studento pavardė
 public:
+
+    /**
+     * @brief Numatytaisiais parametrais inicializuojantis konstruktorius.
+     *
+     * Sukuria objektą su tuščiu vardu ir pavarde.
+     */
     Zmogus() : vard(""), pav("") {}
+    /**
+     * @brief Konstruktorius su vardu ir pavarde.
+     * @param v Vardas
+     * @param p Pavardė
+     */
     Zmogus(const string& v, const string& p) : vard(v), pav(p) {}
 
+    /**
+     * @brief Virtualus destruktorius.
+     *
+     * Leidžia saugiai naikinti objektus per bazinės klasės rodykles.
+     */
     virtual ~Zmogus() = default;
 
     string getVard() const { return vard; }
@@ -20,3 +43,4 @@ public:
 
     virtual void info() const = 0;
 };
+
