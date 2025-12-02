8667fb68154eba7ef9c03e11fcbb22851b397ba3
Kamil-Grigorovic <kamil.grigorovic@gmail.com>
2025-12-03 00:06:58 +0200

    Delete docs directory

--- docs/html/files_dup.js
+++
@@ -1,5 +0,0 @@
-var files_dup =
-[
-    [ "base.h", "base_8h_source.html", null ],
-    [ "new_main.h", "new__main_8h_source.html", null ]
-];
--- docs/html/search/typedefs_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['paz_5ftype_0',['paz_type',['../class_studentas.html#ab9941e3e29893b2db9c216b759b43a0b',1,'Studentas']]]
-];
