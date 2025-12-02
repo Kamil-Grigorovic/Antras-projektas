06f0ceb7e4e755e27c555fce2812f8b72b4d6263
Kamil-Grigorovic <kamil.grigorovic@gmail.com>
2025-12-03 00:10:03 +0200

    Delete html directory

--- html/search/all_0.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['getegzas_0',['getEgzas',['../class_studentas.html#a6d7c4e7fcd81b81f328044ecafa0b8ca',1,'Studentas']]],
-  ['getmediana_1',['getMediana',['../class_studentas.html#a58e0282a49e6481fd6f102200ea34da1',1,'Studentas']]],
-  ['getpav_2',['getPav',['../class_zmogus.html#a89891b9740332d95e1ccbe4b07b66fb1',1,'Zmogus']]],
-  ['getpaz_3',['getPaz',['../class_studentas.html#ae097482ea50be5d175395f308a33aa25',1,'Studentas']]],
-  ['getrez_4',['getRez',['../class_studentas.html#a4b8db911e406947e5a966904aec423e1',1,'Studentas']]],
-  ['getvard_5',['getVard',['../class_zmogus.html#a755a469b04dc1d566264f826b4698244',1,'Zmogus']]]
-];
--- html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['info_0',['info',['../class_zmogus.html#a9c1be5dbdc6d42c022039321ec2b210c',1,'Zmogus::info()'],['../class_studentas.html#adf15d31cf729b51cd5b8412b03bf9523',1,'Studentas::info()']]]
-];
