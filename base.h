62b96ccd9a774cbdf2cad168d3c63dd1e5147496
Kamil-Grigorovic <kamil.grigorovic@gmail.com>
2025-12-22 00:54:23 +0200

    Refactor Zmogus class with protected members and defaults
    Updated Zmogus class to use protected access for member variables and modified constructor to have default parameters. Enhanced destructor to clear member variables.

--- base.h
+++ base.h
@@ -10,30 +10,26 @@ using namespace std;
  * Turi grynai virtualią funkciją info(), todėl negali būti sukurta tiesiogiai.
  */
 class Zmogus {
-private:
+protected:
     string vard; ///< Studento vardas
     string pav; ///< Studento pavardė
 public:
-
-    /**
-     * @brief Numatytaisiais parametrais inicializuojantis konstruktorius.
-     *
-     * Sukuria objektą su tuščiu vardu ir pavarde.
-     */
-    Zmogus() : vard(""), pav("") {}
     /**
      * @brief Konstruktorius su vardu ir pavarde.
      * @param v Vardas
      * @param p Pavardė
      */
-    Zmogus(const string& v, const string& p) : vard(v), pav(p) {}
+    Zmogus(const string& v = "", const string& p = "") : vard(v), pav(p) {}
 
     /**
      * @brief Virtualus destruktorius.
      *
      * Leidžia saugiai naikinti objektus per bazinės klasės rodykles.
      */
-    virtual ~Zmogus() = default;
+    virtual ~Zmogus() {
+        vard.clear();
+        pav.clear();
+    }
 
     /**
      * @brief Grąžina žmogaus vardą.
@@ -64,4 +60,3 @@ public:
      */
     virtual void info() const = 0;
 };
-
